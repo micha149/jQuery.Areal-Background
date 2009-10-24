3d3c4ed2e767bcb7b3412322a2c6cbe1409df3ea
micha149 <michael@van-engelshoven.de>
2009-10-24 16:18:27 +0000

    Added header comment with license informations

--- plugin/jquery.areal-background.js
+++ plugin/jquery.areal-background.js
@@ -1,3 +1,14 @@
+/*
+ * Areal background plugin for jQuery
+ * http://michael-van-engelshoven.de/projects/jquery/areal-background
+ *
+ * Copyright (c) 2009 Michael van Engelshoven
+ * Licensed under the GNU General Public License, version 2.
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * Date: Sat, 24 Oct 2009
+ * Version: 0.1
+ */
 jQuery.fn.arealBackground = function() {
   return jQuery(this).each(function() {
     var elem = jQuery(this);
